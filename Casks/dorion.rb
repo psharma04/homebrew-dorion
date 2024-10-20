005762b6b61497e9cde7a55c47e74bda57d5f48b
Pepsi <git@pshar.ma>
2024-10-20 22:59:28 +1100

    Add Dorion cask formula

---
+++ Casks/dorion.rb
@@ -0,0 +1,32 @@
+cask "dorion" do
+    version "6.0.0"
+  
+    if Hardware::CPU.intel?
+      url "https://github.com/SpikeHD/Dorion/releases/download/v#{version}/Dorion_#{version}_x64.dmg"
+    else
+      url "https://github.com/SpikeHD/Dorion/releases/download/v#{version}/Dorion_#{version}_aarch64.dmg"
+    end
+  
+    name "Dorion"
+    desc "Dorion application"
+    homepage "https://github.com/SpikeHD/Dorion"
+  
+    livecheck do
+      url "https://github.com/SpikeHD/Dorion/releases/latest"
+      strategy :page_match do |page|
+        v = page[%r{href=.*?/Dorion/releases/download/v?(\d+(?:\.\d+)*)/Dorion_.*\.dmg}i, 1]
+        v.tr("_", ".")
+      end
+    end
+  
+    app "Dorion.app"
+  
+    auto_updates true
+  
+    zap trash: [
+      "~/Library/Application Support/Dorion",
+      "~/Library/Preferences/com.dorion.plist",
+      "~/Library/Saved Application State/com.dorion.savedState",
+    ]
+  end
+  
