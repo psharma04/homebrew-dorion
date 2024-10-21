1621a8a85533ea1776ba2b00ff35ccac12f67b1c
Pepsi <git@pshar.ma>
2024-10-21 12:36:58 +1100

    add shasums

--- Casks/dorion.rb
+++ Casks/dorion.rb
@@ -1,11 +1,13 @@
 cask "dorion" do
-  version "6.2.0"
+  version "6.0.0"
 
   on_arm do
     url "https://github.com/SpikeHD/Dorion/releases/download/v#{version}/Dorion_#{version}_aarch64.dmg"
+    sha256 "1c67184aa1b2ab3fcb2a3eed9560a6aa20af6cf57d96aeaa733cd820decee197"
   end
   on_intel do
     url "https://github.com/SpikeHD/Dorion/releases/download/v#{version}/Dorion_#{version}_x64.dmg"
+    sha256 "de0c7e0994e2a98681fd87e6e74a340fa5a50a7f89bd8cad0eddc22630773703"
   end
 
   name "Dorion"
